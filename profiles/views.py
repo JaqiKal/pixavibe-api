b892dfc1cc3afbe63d793e2f7493cabd5d359f6a
Jacqueline Kalmár <jaqika@gmail.com>
2024-05-23 09:05:36 +0000

    (feat): Add Profiles serializer and list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+# converts model instances to JSON and vice versa
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(APIView):
+    """
+    List all profiles
+    No Create view (post method), as profile creation handled by django
+    signals.
+    This class-based view provides a read-only endpoint for listing all
+    profiles in the system. By inheriting from APIView, it leverages Django
+    REST Framework's features to handle HTTP GET requests, retrieve data
+    from the database, serialize it into JSON format, and send it back in
+    the HTTP response. Profile creation is managed by Django signals,
+    hence no post method is included in this view.
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
