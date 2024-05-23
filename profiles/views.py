a99239d14fded8a2ef91e15c121ab9036b6b67ab
Jacqueline Kalmár <jaqika@gmail.com>
2024-05-23 10:11:52 +0000

    (feat): Add profiles detail view and PEP8 validate

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -21,3 +23,43 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Retrieve, update or delete a profile instance.
+    """
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        
+        """
+        Retrieve a profile by its primary key (pk).
+        """
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        
+        """
+        Handle GET request for retrieving a specific profile.
+        """
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+        
+
+    def put(self, request, pk):
+        
+        """
+        Handle PUT request for updating a specific profile.
+        """
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
