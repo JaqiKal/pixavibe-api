57a46ff94dae41b95c7c07ca04c9941c2f6249b6
Jacqueline Kalmár <jaqika@gmail.com>
2024-05-25 11:38:00 +0000

    feat: Add Like serializer

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,23 +1,15 @@
 from rest_framework import serializers
-from .models import Likes
+from likes.models import Like
 
 
-class LikesSerializer(serializers.ModelSerializer):
+class LikeSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Likes model
-    Adds three extra fields when returning a list of Comment instances
+    Serializer for the Like model. The create method handles
+    the unique constraint on 'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-    post = serializers.ReadOnlyField(source='owner.profile.id')
-    created_at= serializers.DateTimeField(source='owner.profile.image.url')
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
 
     class Meta:
-        model = Comment
-        fields = [
-            'id', 'owner', 'is_owner', 'post', 'created_at'
-        ]
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
+
