5e542fb10768b0a39d2d9573fcb7296603da661e
Jacqueline Kalmár <jaqika@gmail.com>
2024-05-26 16:37:23 +0000

    feat: Prepare API for deployment
    Add functionality to create a database and a Heroku app.
    
    Update IDE settings to connect to an external database.
    
    Execute database migrations to the new database.
    
    Add Procfile with commands for Heroku to build and run the project.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -35,18 +36,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sj79#kfhhd=gq1eh87@6lm&kaxi_nd%d=n1$np_@k8lqydqymi'
+ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# This mean it is True in development, and False in production
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    '8000-jaqikal-pixavibeapi-om5jvlka210.ws-eu114.gitpod.io',
+    'localhost'
     '.herokuapp.com',
     '127.0.0.1',
-    '.gitpod.io'
-]
+    '.gitpod.io',
 
+]
 
 # Application definition
 
@@ -68,6 +70,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
 
     # App
     'profiles',
@@ -107,6 +110,7 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 # Declare cookie names for the access and refresh tokens
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+ JWT_AUTH_SAMESITE = 'None'
 
 # Overwrite the default USER_DETAILS_SERIALIZER
 REST_AUTH_SERIALIZERS = {
@@ -115,6 +119,7 @@ REST_AUTH_SERIALIZERS = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,6 +128,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+ if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+ else:
+     CORS_ALLOWED_ORIGIN_REGEXES = [
+         r"^https://.*\.gitpod\.io$",
+     ]
+
+ CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'drf_api.urls'
 
@@ -148,13 +164,17 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+if 'DEV' in os.environ:
+     DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+     DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
