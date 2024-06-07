42b48e8d3c78a28fd9895ba7f65a4789c7c2b40a
Jacqueline Kalmár <jaqika@gmail.com>
2024-06-07 10:34:01 +0000

    feat: Add max_length 255 to content field in Contact model
    Update model to include better content management.

--- contacts/models.py
+++ contacts/models.py
@@ -13,7 +13,7 @@ class Contact(models.Model):
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     reason = models.CharField(max_length=50)
-    content = models.TextField()
+    content = models.TextField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
